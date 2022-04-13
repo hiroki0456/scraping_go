f8feeb6d853a9c9d1022ecac038734af3ec7cc9a
hiroki0456 <volleyball0456@gmail.com>
2022-04-13 23:21:32 +0900

    tourist entity

---
+++ entity/tourist_area.go
@@ -0,0 +1,13 @@
+package entity
+
+type TouristArea struct {
+	Rank              uint32
+	JoyfulScore       float64
+	JoyfulContent     string
+	CongestionScore   float64
+	CongestionContent string
+	SceneryScore      float64
+	SceneryContent    string
+	AccessScore       float64
+	AccessContent     string
+}
